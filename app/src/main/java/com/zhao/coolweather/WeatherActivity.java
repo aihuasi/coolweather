98151bdbed89f696c5f91dde7728e405486287af
zbzk <1802252315@qq.com>
2017-08-14 12:08:20 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/zhao/coolweather/WeatherActivity.java
+++ app/src/main/java/com/zhao/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.zhao.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.zhao.coolweather.gson.Forecast;
 import com.zhao.coolweather.gson.Weather;
+import com.zhao.coolweather.service.AutoUpdateService;
 import com.zhao.coolweather.util.HttpUtil;
 import com.zhao.coolweather.util.Utility;
 
@@ -213,6 +215,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
